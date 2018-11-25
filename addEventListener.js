4775962fa988168d929c6fd5cc952469642427b2
mhe18 <mhe18@jhu.edu>
2018-11-25 15:37:17 -0500

    branch test

--- addEventListener.js
+++ addEventListener.js
@@ -3,7 +3,10 @@ var buffer={}; // store the tabs
 chrome.tabs.onActivated.addListener(function (activeInfo) {
     chrome.tabs.get(activeInfo.tabId, function callback(tab){
         var url = new URL(tab.url);
-        getScreenshot(tab);
+        var result = getScreenshot(tab);
+        chrome.tabs.sendMessage(tab.id,{image : result} function(response) {
+            console.log(response.confirmation);
+        });
     })
 });
 
@@ -12,4 +15,17 @@ chrome.tabs.onRemoved.addListener(function(tabId, removeInfo));
 chrome.tabs.onCreated.addListener(function(tab));
 
 chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab));
+
+                                  
+        
+
+/*chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.requested == "image"){
+            var overlay = document.createElement('div');
+            overlay.setAttribute("style","width:100%;height:100%");
+            //Code to create the div
+            sendResponse({confirmation: "Successfully created div"});
+        }
+    });*/
                                      
--- tab.js
+++ tab.js
@@ -14,10 +14,9 @@ function getScreenshot(tab){
 function compare(dataUrl, id){
     resemble(dataUrl).compareTo(buffer[id]).onComplete(function(data){
         var percent=data.misMatchPercentage;
-        if(percent < Threshold){
-        }
-        else{
-            //draw
+        if(percent > Threshold){
+            var diffImage = document.getElementById('res_img');
+		    diffImage.src = data.getImageDataUrl();
         }
         chrome.pageAction.show(id);
         buffer[id]=dataUrl;
