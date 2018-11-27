6ca4867cb3165241c908ee4160dde8d8b5e1ffea
mhe18 <mhe18@jhu.edu>
2018-11-26 23:18:12 -0500

    'webpage'

--- tab.js
+++ tab.js
@@ -6,7 +6,23 @@ function stop_thread(){
 function remove(tabId) {
     delete buffer[tabId];
 }
-
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.requested == "greeting"){
+            var result = document.createElement('div');
+            result.id="result";
+            result.style.width = "100%";
+            result.style.height = "100%";
+            result.style.backgroundImage=img;
+            result.addEventListener("click", function(){
+                var temp = document.getElementById('result')
+                    temp.parentNode.removeChild(temp);
+            });
+            
+            sendResponse({confirmation: "Successfully created div"});
+            document.body.appendChild(result);
+        }
+    });
 function getScreenshot(tab){
     thread = setInterval(function(){
         chrome.tabs.captureVisibleTab({format : "png"}, function(dataUrl){
@@ -25,35 +41,36 @@ function getScreenshot(tab){
                 //console.log("asadada");
             }
         })        
-    },1000);
+    },500);
 }
 
 function compare(dataUrl, id){
     resemble(dataUrl).compareTo(buffer[id]).onComplete(function(data){
         var percent=data.misMatchPercentage;
         img = data.getImageDataUrl();
-        //console.log(img);
+        console.log(img);
         var icon;
-        //console.log(data.misMatchPercentage);
-        //console.log(data.getImageDataUrl());
+        console.log(data.misMatchPercentage);
+        console.log(data.getImageDataUrl());
         if(percent > Threshold){
             //var diffImage = document.getElementById('res_img');
 		    //img = data.getImageDataUrl();
+            chrome.tabs.sendMessage(id,{greeting: "hello"}, function(response){
+            console.log('111');
             icon={tabId:id, path:"image/unsafe.png"};
+            chrome.pageAction.setIcon(icon);
+        });
+
         }
         else{
             //var diffImage = document.getElementById('res_img');
 		    //img = data.getImageDataUrl();
             icon={tabId:id, path:"image/safe.png"};
+             chrome.pageAction.setIcon(icon);
         }
-        chrome.pageAction.setIcon(icon);
+       
         chrome.pageAction.show(id);
         //buffer[id]=dataUrl;
         
     })
-    
-
-    
-
-    
 }
